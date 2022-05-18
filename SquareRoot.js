43a553aedfd8777a9a84aa2cd3fe8335ee45e438
Sriram.Mahadevan <srirammahadevan@outlook.com>
2022-05-18 13:37:39 +0530

    fixed an issue

--- SquareRoot.js
+++ SquareRoot.js
@@ -3,12 +3,11 @@ function sqrt(num) {
     var l=0,u=num;
     while(sqrt>0) {
         sqrt=(l+u)/2;
-        console.log(sqrt);
-        if(parseInt(sqrt*sqrt)==num) break;
+        if((sqrt*sqrt).toFixed(2)==num) break;
         else if(sqrt*sqrt>num) u=sqrt-1;
         else if(sqrt*sqrt<num) l=sqrt+1;
     }
     return sqrt;
 }
 
-console.log(sqrt(70));
+console.log(sqrt(100).toFixed(2));
